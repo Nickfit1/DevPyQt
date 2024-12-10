a0831669cdf22b5af31899ca25c0afc659cd5ce6
Nickfit1 <nickfit@yandex.ru>
2024-12-11 00:18:09 +0300

    lab2.1

--- scripts/lecture_1/part_1/a_first_app.py
+++ scripts/lecture_1/part_1/a_first_app.py
@@ -1,8 +1,48 @@
-import sys
+from PySide6 import QtWidgets
 
-from PySide6.QtWidgets import QApplication
 
-app = QApplication(sys.argv)
-app.exec()
 
-print("Приложение закрыто!")
+class Window(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self) -> None:
+        """
+        Доинициализация Ui
+
+        :return: None
+        """
+
+        self.pushButton = QtWidgets.QPushButton("Выполнить что-то")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.pushButton)
+
+        self.setLayout(layout)
+
+    def initSignals(self) -> None:
+        """
+        Инициализация сигналов
+
+        :return: None
+        """
+
+        # Указываем, что при нажатии на кнопку,
+        # будут выполнены дествия описанные
+        # в методе класса onPushButtonClicked
+        self.pushButton.clicked.connect(self.onPushButtonClicked)
+
+    def onPushButtonClicked(self):
+        print("pushButton was clicked")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication()
+
+    window = Window()
+    window.show()
+
+    app.exec()
--- scripts/practice_2/b_laboratory/b_add_signals.py
+++ scripts/practice_2/b_laboratory/b_add_signals.py
@@ -9,7 +9,7 @@ class Window(QtWidgets.QWidget):
         super().__init__(parent)
 
         self.initUi()
-        # TODO: Вызвать метод с инициализацией сигналов
+        self.initSignals()# TODO: Вызвать метод с инициализацией сигналов
 
     def initUi(self) -> None:
         """
@@ -132,7 +132,7 @@ class Window(QtWidgets.QWidget):
         :return: None
         """
 
-        self.pushButtonComboBox  # TODO подключить слот для вывода текста из comboBox в plainTextEditLog при нажатии на кнопку
+        self.pushButtonComboBox.connect()  # TODO подключить слот для вывода текста из comboBox в plainTextEditLog при нажатии на кнопку
         self.pushButtonLineEdit.clicked.connect(self.onPushButtonLineEditClicked)
         self.pushButtonTextEdit  # TODO подключить слот для вывода текста из textEdit в plainTextEditLog при нажатии на кнопку
         self.pushButtonPlainTextEdit  # TODO подключить слот для вывода текста из plaineTextEdit в plainTextEditLog при нажатии на кнопку
